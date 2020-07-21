1ab6fd3abe182418030410b214ffdcc79a7419df
Sergey Khabarov <delkuenta@gmail.com>
2020-07-22 00:22:52 +0300

    1. Пример файла txt с переменными 2. Данные логина перенесены в отдельный файл

---
+++ userdata/login_data.py
@@ -0,0 +1,8 @@
+class LoginData:
+    Mr_KSUP_Seller = "https://Mr_KSUP_Seller:AsdGhj-5681-Sle@ksup-tst.lanit/_windows/default.aspx?ReturnUrl=%2f_layouts%2f15%2fAuthenticate.aspx%3fSource%3d%252F%255Fwindows%252Fdefault%252Easpx&Source=%2f_windows%2fdefault.aspx"
+    Mr_KSUP_Dir = "https://Mr_KSUP_Dir:AsdGhj-5681-Dri@ksup-tst.lanit/_windows/default.aspx?ReturnUrl=%2f_layouts%2f15%2fAuthenticate.aspx%3fSource%3d%252F%255Fwindows%252Fdefault%252Easpx&Source=%2f_windows%2fdefault.aspx"
+    Mr_KSUP_Fin = "https://Mr_KSUP_Fin:AsdGhj-5681-Fni@ksup-tst.lanit/_windows/default.aspx?ReturnUrl=%2f_layouts%2f15%2fAuthenticate.aspx%3fSource%3d%252F%255Fwindows%252Fdefault%252Easpx&Source=%2f_windows%2fdefault.aspx"
+    Mr_KSUP_Count = "https://Mr_KSUP_Count:*AF5hcnEfF8D2g8a@ksup-tst.lanit/_windows/default.aspx?ReturnUrl=%2f_layouts%2f15%2fAuthenticate.aspx%3fSource%3d%252F%255Fwindows%252Fdefault%252Easpx&Source=%2f_windows%2fdefault.aspx"
+    Mr_KSUP_Legal = "https://Mr_KSUP_Legal:AsdGhj-5681-Lge@ksup-tst.lanit/_windows/default.aspx?ReturnUrl=%2f_layouts%2f15%2fAuthenticate.aspx%3fSource%3d%252F%255Fwindows%252Fdefault%252Easpx&Source=%2f_windows%2fdefault.aspx"
+    Mr_KSUP_UDPRPO = "https://Mr_KSUP_UDPRPO:lPRvCNi9m9zU8gb@ksup-tst.lanit/_windows/default.aspx?ReturnUrl=%2f_layouts%2f15%2fAuthenticate.aspx%3fSource%3d%252F%255Fwindows%252Fdefault%252Easpx&Source=%2f_windows%2fdefault.aspx"
+    Mr_KSUP_KKP = "https://Mr_KSUP_KKP:su@rMpuYu^bOI5Z@ksup-tst.lanit/_windows/default.aspx?ReturnUrl=%2f_layouts%2f15%2fAuthenticate.aspx%3fSource%3d%252F%255Fwindows%252Fdefault%252Easpx&Source=%2f_windows%2fdefault.aspx"
