eb3f0335b03651c9a3b6715b5bfd46bf4adf35a8
Sergey Khabarov <delkuenta@gmail.com>
2020-07-21 17:27:53 +0300

    Данные хранятся в отдельном файле

---
+++ userdata/user_data.py
@@ -0,0 +1,29 @@
+class UserData:
+    #Данные для создания пресейловой активности
+    name_presale = "t1e1_тест_ПА_тендер_самостПрод_катА"
+    type_presale = "Тендерная заявка"
+    customer = "ООО \"ДЕКОР\""
+    divisions_seller = "ОНЛАНТА"
+    division_performer = "ОНЛАНТА"
+    performer_responsible = "Бравосов Андрей Игоревич"
+    performer_legal = "ООО \"ОНЛАНТА\""
+    sale_law_type = "44-ФЗ"
+    group_category = "TIE_6"
+    # TIE_6 = Программное обеспечение. LBL_7 = Cтроительство. LBL_5 = Обучение.
+    # LBL_4 = Дистрибуция и ритейл. LBL_3 = Аппаратное обеспечение и инфраструктура
+    category = "Разработка заказного ПО"
+    sum = 52000000
+    currency = "Доллар"
+    application_size = "1000"
+    contract_size = "2000"
+    separate_sale = "Да"
+    competition_deadline_From = "15.07.2020"
+    plan_date_contract_conclusion = "1.07.2020"
+    plan_Date_contract_finish = "30.12.2020"
+    project_probability = "100"
+    description_plain_text = "Краткое описание текст"
+    risks = "Текст_риски_тест"
+    yeartable_1line = 2020
+    yeartable_2line = 2020
+    quarter_1line = "3 квартал"
+    quarter_2line = "4 квартал"
